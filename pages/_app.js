b5d2605ac90e52d32dc7c8e77c44ffe53332855f
delinuxist <delinuxist@gmail.com>
2022-06-13 02:07:31 +0000

    added AuthProvider and Toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Navbar from "../components/layouts/Navbar";
+import { AuthContextProvider } from "../context/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <ToastContainer position="top-center" />
+      <Navbar />
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;

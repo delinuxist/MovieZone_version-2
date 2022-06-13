f194f34aa215fe165d342fcba8b7859b4d9680c4
delinuxist <delinuxist@gmail.com>
2022-06-13 02:08:41 +0000

    row component

---
+++ components/Row.jsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+const Row = ({ title, fetchUrl, rowID }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchUrl).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchUrl]);
+
+  const slideLeft = () => {
+    let slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    let slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  return (
+    <>
+      <h2 className=" font-bold md:text-xl p-4">{title}</h2>
+      <div className=" relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={40}
+          className=" bg-white text-black rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
+        <div
+          id={"slider" + rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative "
+        >
+          {movies.map((movie, id) => (
+            <Movie key={id} movie={movie} />
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          size={40}
+          className=" bg-white text-black rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block right-0"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Row;

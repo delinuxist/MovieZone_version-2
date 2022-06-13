5a9d5c7b8be1f058bb3944bcff597c9e72828b0f
delinuxist <delinuxist@gmail.com>
2022-06-13 02:07:09 +0000

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
+import Hero from "../components/Hero";
+import requests from "../components/Request";
+import Row from "../components/Row";
 
 export default function Home() {
   return (
@@ -7,6 +9,14 @@ export default function Home() {
       <Head>
         <title>MovieZone</title>
       </Head>
+      <>
+        <Hero />
+        <Row rowID="1" title="UpComing" fetchUrl={requests.requestUpcoming} />
+        <Row rowID="2" title="Popular" fetchUrl={requests.requestPopular} />
+        <Row rowID="3" title="Trending" fetchUrl={requests.requestTrending} />
+        <Row rowID="4" title="Top Rated" fetchUrl={requests.requestTopRated} />
+        <Row rowID="5" title="Horror" fetchUrl={requests.requestHorror} />
+      </>
     </div>
   );
 }

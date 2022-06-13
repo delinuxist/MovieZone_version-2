8de0ccebaeed0ec9f93819103695591146cb0048
delinuxist <delinuxist@gmail.com>
2022-06-13 02:06:22 +0000

    signup page

---
+++ pages/signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Signup from "../components/views/Signup";
+
+const signup = () => {
+  return (
+    <>
+      <Signup />
+    </>
+  );
+};
+
+export default signup;

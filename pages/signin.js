3d821945aec92c429d3943f893ee6bb7ceca9850
delinuxist <delinuxist@gmail.com>
2022-06-13 02:07:59 +0000

    signin page

---
+++ pages/signin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../components/views/Login";
+
+const signin = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default signin;
